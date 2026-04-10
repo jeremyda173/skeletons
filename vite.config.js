1b927e3e8fb7398873994a161b6437b796588a46
jeremyda173 <jeremygithud173@gmail.com>
2026-04-09 22:25:31 -0400

    chore: configure path aliases (@ points to src)

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-import { resolve } from 'path'
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
   build: {
     lib: {
-      // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, 'src/index.jsx'),
       name: 'SkeletonUI',
-      // the proper extensions will be added
       fileName: 'skeleton-ui',
     },
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
       external: ['react', 'react-dom', 'styled-components'],
       output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
         globals: {
           react: 'React',
           'react-dom': 'ReactDOM',
